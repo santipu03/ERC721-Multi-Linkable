187717d90c038dc7a46fc0c30dae23296bb0b449
santipu <santipujolsau@gmail.com>
2023-01-23 19:35:53 +0100

    added ChakraProvider to App

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { MoralisProvider } from "react-moralis";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MoralisProvider initializeOnMount={false}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </MoralisProvider>
   </React.StrictMode>
 );
