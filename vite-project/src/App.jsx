121747a5060c04d66041eb45e2122cb1a7d2bac5
santipu <santipujolsau@gmail.com>
2023-01-23 18:36:38 +0100

    set up moralisProvider, set reset CSS for all elements and include main components in App·

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Box } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Box w="100vw" h="100vh" bg={"#f8f9fa"} overflowX={"hidden"}>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </Box>
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { MoralisProvider } from "react-moralis";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MoralisProvider initializeOnMount={false}>
+      <App />
+    </MoralisProvider>
+  </React.StrictMode>
+);
