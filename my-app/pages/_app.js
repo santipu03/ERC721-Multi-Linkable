86e6b82c16bc47c8c2ccc35ab2781f00be732d80
santipu <santipujolsau@gmail.com>
2023-01-23 17:36:57 +0100

    installed dependencies of chakraUI and solved bug in _app.js

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -2,7 +2,9 @@ import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  <ChakraProvider>
-    return <Component {...pageProps} />;
-  </ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 }
