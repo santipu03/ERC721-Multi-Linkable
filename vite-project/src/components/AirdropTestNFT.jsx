c8dcf91eadde2bfd4fa7a8640466cc6dfe0ea94e
santipu <santipujolsau@gmail.com>
2023-01-24 16:29:38 +0100

    created main functionality of the section for minting the E7L

---
+++ vite-project/src/components/AirdropTestNFT.jsx
@@ -0,0 +1,17 @@
+import { Heading, Text, Button } from "@chakra-ui/react";
+
+function AirdropTestNFT() {
+  return (
+    <>
+      <Heading>Receive a Test NFT to Link</Heading>
+      <Text>
+        For testing purposes, you will receive an NFT ERC721 for the linking
+      </Text>
+      <Button colorScheme="teal" variant="solid">
+        Receive NFT
+      </Button>
+    </>
+  );
+}
+
+export default AirdropTestNFT;
