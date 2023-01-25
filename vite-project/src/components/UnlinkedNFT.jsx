41e1a0cdf45af41d93b983c356c7edcd0b5fa712
santipu <santipujolsau@gmail.com>
2023-01-25 17:35:35 +0100

    created components for linked and unlinked E7ML and set up basic structure for them

---
+++ vite-project/src/components/UnlinkedNFT.jsx
@@ -0,0 +1,41 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
+
+function UnlinkedNFT({ nft }) {
+  return (
+    <Flex
+      flexDir={"column"}
+      border={"1px solid grey"}
+      borderRadius={"10px"}
+      height={"250px"}
+      width={"250px"}
+      bgColor={"#f8f9fa"}
+      justifyContent={"space-between"}
+    >
+      <Flex
+        width={"100%"}
+        height={"180px"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Image
+          src={nft.rawMetadata.image}
+          alt="img"
+          objectFit={"contain"}
+          height={"100%"}
+          width={"100%"}
+          borderRadius={"10px"}
+        ></Image>
+      </Flex>
+      <Flex flexDir={"column"} padding={"0 10px"}>
+        <Text>
+          <b>{nft.tokenId}</b>
+        </Text>
+        <Text margin={0}>
+          <b>{nft.contract.name}</b>
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default UnlinkedNFT;
