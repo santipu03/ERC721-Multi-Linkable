7b8b6186e4e6ff2881e6ba5db8840df4c1ab055a
santipu <santipujolsau@gmail.com>
2023-01-26 13:08:37 +0100

    implemented URI functions in TOKEN1 contract to show image in frontend

--- hardhat/contracts/test/Token1.sol
+++ hardhat/contracts/test/Token1.sol
@@ -4,13 +4,26 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Token1 is ERC721 {
+    string private URI = "Qme8miFUJmsCgPU3wrz1afDzhysJBUGDbKMu5x8JzVLY3f";
+
     constructor() ERC721("Token1", "T1") {}
 
     function mint(uint256 tokenId) public {
         _safeMint(msg.sender, tokenId);
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "Qme8miFUJmsCgPU3wrz1afDzhysJBUGDbKMu5x8JzVLY3f";
+    function _baseURI() internal view override returns (string memory) {
+        return URI;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        return URI;
     }
 }
